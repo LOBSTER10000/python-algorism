2fcfb7efd9abe1e619afb927e12e7a4dddd3972b
랍스타이동규 <ghh112034@gmail.com>
2024-12-12 17:46:49 +0900

    Create base.py

---
+++ base.py
@@ -0,0 +1,14 @@
+print("print's is good")
+
+print('"print" is good')
+
+print(f"Python \n is good")
+
+
+# escape code
+
+# \n (문자열 안에서 줄을 바꿀 때 사용)
+# \t (문자열 사이에 탭 간격을 줄 때 사용)
+# \\ (\를 문자열 그대로 사용할 때 사용)
+# \b (백스페이스 사용)
+
